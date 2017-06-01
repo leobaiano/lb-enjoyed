7e23f404b9e2a2af496590b412bae9e85cf19b13
Leo Baiano <ljunior2005@gmail.com>
2017-06-01 12:10:15 -0300

    include file class widget favorite list

--- lb-enjoyed.php
+++ lb-enjoyed.php
@@ -70,6 +70,9 @@ class LB_Enjoyed {
 		add_action( 'wp_ajax_nopriv_get_favorite_list', array('\LBEnjoyed\Favorite_API', 'get_favorite_list' ) );
 		add_action( 'wp_ajax_get_favorite_list', array('\LBEnjoyed\Favorite_API', 'get_favorite_list' ) );
 
+		// Add Widget Favorite List
+		add_action( 'widgets_init', array( '\LBEnjoyed\Widget_Favorite_List', 'register_widget' ) );
+
 		// Hook so other developers can extend the plugin
 		do_action( 'lb_enjoyed_loaded' );
 	}
@@ -122,6 +125,7 @@ class LB_Enjoyed {
 	public function includes() {
 		include_once( 'class/class-favorite.php' );
 		include_once( 'class/class-favorite-api.php' );
+		include_once( 'class/class-widget-favorite-list.php' );
 	}
 
 } // end class LB_Enjoyed();
