7d6dab1f381a79f45633662fe7611835a787561d
Leo Baiano <ljunior2005@gmail.com>
2017-06-01 10:11:33 -0300

    Refactory method for insert favorite icon and create method for genarate template HTML for favorite icon

--- class/class-favorite.php
+++ class/class-favorite.php
@@ -20,26 +20,12 @@ class Favorite {
 	 * @since  0.1.0
 	 */
 	public static function hook_insert_favorite_icon_in_post_content( $content ) {
-		$favorite_content = '';
-		$favorite_icon = apply_filters( 'lb_favorite_icon', 'grade' );
 		$favorite_position = apply_filters( 'lb_favorite_location_icon', 'after' );
-		$css_class = apply_filters( 'lb_favorite_container_classes_css', array( 'lb_favorite_container' ) );
-		$css_class = implode( ' ', $css_class );
-
 		if ( $favorite_position !== 'after' && $favorite_position !== 'before' && $favorite_position !== 'both' ) {
 			$favorite_position = 'after';
 		}
 
-		// Block with the content of the favorite icon
-		$favorite_content .= '<div class="' . $css_class . '">';
-			if ( ! is_bool( self::chek_if_post_is_favorite( get_the_ID(), self::get_favorites() ) ) ) {
-				$favorite_content .= '<a href="javascript:;" title="' . __( 'Favorite post', 'lb-enjoyed' ) . '" class="active" data-post-id="' . get_the_ID() . '">';
-			} else {
-				$favorite_content .= '<a href="javascript:;" title="' . __( 'Favorite post', 'lb-enjoyed' ) . '" data-post-id="' . get_the_ID() . '">';
-			}
-				$favorite_content .= '<i class="material-icons">' . $favorite_icon . '</i> ' . __( 'Favorite', 'lb-enjoyed' );
-			$favorite_content .= '</a>';
-		$favorite_content .= '</div>';
+		$favorite_content = self::genarate_template_html_for_favorite_icon( get_the_ID() );
 
 		// Return the content of the post with the favorite icon
 		switch ( $favorite_position ) {
@@ -55,6 +41,31 @@ class Favorite {
 				break;
 		}
 		return $content;
+	}
+
+	/**
+	 * Template HTML for view favorite icon
+	 *
+	 * @param int $post_id POST id
+	 * @return strin Template HTML
+	 */
+	public static function genarate_template_html_for_favorite_icon( $post_id ) {
+		$favorite_content = '';
+		$favorite_icon = apply_filters( 'lb_favorite_icon', 'grade' );
+		$css_class = apply_filters( 'lb_favorite_container_classes_css', array( 'lb_favorite_container' ) );
+		$css_class = implode( ' ', $css_class );
+
+		$favorite_content .= '<div class="' . $css_class . '">';
+			if ( ! is_bool( self::chek_if_post_is_favorite( get_the_ID(), self::get_favorites() ) ) ) {
+				$favorite_content .= '<a href="javascript:;" title="' . __( 'Favorite post', 'lb-enjoyed' ) . '" class="active" data-post-id="' . get_the_ID() . '">';
+			} else {
+				$favorite_content .= '<a href="javascript:;" title="' . __( 'Favorite post', 'lb-enjoyed' ) . '" data-post-id="' . get_the_ID() . '">';
+			}
+				$favorite_content .= '<i class="material-icons">' . $favorite_icon . '</i> ' . __( 'Favorite', 'lb-enjoyed' );
+			$favorite_content .= '</a>';
+		$favorite_content .= '</div>';
+
+		return $favorite_content;
 	}
 
 	/**
