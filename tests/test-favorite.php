7b1e61ad26a818508b086a5c76ae990880bd839a
Leo Baiano <ljunior2005@gmail.com>
2017-06-01 14:37:49 -0300

    fix

---
+++ tests/test-favorite.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class Favorite_Test
+ *
+ * @package LBEnjoyed
+ */
+
+namespace LBEnjoyed\Favorite\Test;
+use LogFavorite\Favorite;
+
+class FavoriteTest extends \WP_UnitTestCase {
+
+}
