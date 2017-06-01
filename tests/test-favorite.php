2bae89e9905d56602b61db7e87d0c3c3fe2597e5
Leo Baiano <ljunior2005@gmail.com>
2017-06-01 14:56:20 -0300

    update files

--- tests/test-favorite.php
+++ tests/test-favorite.php
@@ -9,5 +9,20 @@ namespace LBEnjoyed\Favorite\Test;
 use LogFavorite\Favorite;
 
 class FavoriteTest extends \WP_UnitTestCase {
+	public function setUp()
+    {
+	    parent::setUp();
+        $this->favorites = new \LogFavorite\Favorite();
+	    $_COOKIE['log-favorites'] = 'foo,bar';
 
+    }
+
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
 }
